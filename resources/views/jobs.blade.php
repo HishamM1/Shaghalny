816fbf7778850207c8e7c142218282af2f34b1d0
Executioner47 <faresgalal09@gmail.com>
2022-10-28 23:22:26 +0200

    pagination edited

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -11,9 +11,6 @@
     <link rel="stylesheet" href="/styles/global.css">
     <!-- Css -->
     <link rel="stylesheet" href="/styles/jobs.css" />
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <title>Jobs</title>
 </head>
 
@@ -160,8 +157,10 @@
                         </div>
                     @endforeach
                 </div>
+                {{ $jobs->links() }}
+            </div>
+        </div>
     @endif
-    {{ $jobs->links() }}
     <!-- Jobs Section  -->
     <script src="/scripts/main.js"></script>
 </body>
